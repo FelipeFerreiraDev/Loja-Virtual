d24e978e07ca64ddff978052ad6299cc0dfade7c
FelipeFerreiraDev <felipeferreira.sp.dev@gmail.com>
2021-01-19 16:28:06 -0300

    ExibindobottomSheet

---
+++ lib/screens/edi_product/components/image_source_sheet.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class ImageSourceSheet extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BottomSheet(
+        onClosing: () {},
+        builder: (_) => Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                FlatButton(
+                  onPressed: () {},
+                  child: const Text('Camêra'),
+                ),
+                FlatButton(
+                  onPressed: () {},
+                  child: const Text('Galeria'),
+                ),
+              ],
+            ));
+  }
+}
--- lib/screens/edi_product/components/images_form.dart
+++ lib/screens/edi_product/components/images_form.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:BJDelivery/models/product.dart';
+import 'package:BJDelivery/screens/edi_product/components/image_source_sheet.dart';
 import 'package:carousel_pro/carousel_pro.dart';
 import 'package:flutter/material.dart';
 
@@ -18,7 +19,7 @@ class ImagesForm extends StatelessWidget {
           aspectRatio: 1,
           child: Carousel(
             boxFit: BoxFit.contain,
-            images: state.value.map((image) {
+            images: state.value.map<Widget>((image) {
               return Stack(
                 fit: StackFit.expand,
                 children: <Widget>[
@@ -35,7 +36,7 @@ class ImagesForm extends StatelessWidget {
                   Align(
                     alignment: Alignment.topRight,
                     child: IconButton(
-                        icon: Icon(Icons.remove),
+                        icon: const Icon(Icons.remove),
                         color: Colors.red,
                         onPressed: () {
                           state.value.remove(image);
@@ -44,7 +45,18 @@ class ImagesForm extends StatelessWidget {
                   )
                 ],
               );
-            }).toList(),
+            }).toList()
+              ..add(Material(
+                  color: Colors.grey[100],
+                  child: IconButton(
+                      icon: const Icon(Icons.add_a_photo),
+                      color: Theme.of(context).primaryColor,
+                      iconSize: 50,
+                      onPressed: () {
+                        showModalBottomSheet(
+                            context: context,
+                            builder: (_) => ImageSourceSheet());
+                      }))),
             dotSize: 4,
             dotSpacing: 15,
             dotBgColor: Colors.transparent,
