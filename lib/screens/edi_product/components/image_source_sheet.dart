0e8b51b2e274dc5439c6d0ff479b7c25ae420bb1
FelipeFerreiraDev <felipeferreira.sp.dev@gmail.com>
2021-01-19 17:04:02 -0300

    Exibindo imagem selecionada

--- lib/screens/edi_product/components/image_source_sheet.dart
+++ lib/screens/edi_product/components/image_source_sheet.dart
@@ -1,7 +1,13 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
 class ImageSourceSheet extends StatelessWidget {
+  ImageSourceSheet({this.onImageSelected});
+
+  final Function(File) onImageSelected;
+
   final ImagePicker picker = ImagePicker();
 
   @override
@@ -13,14 +19,18 @@ class ImageSourceSheet extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
                 FlatButton(
-                  onPressed: () {
-                    picker.getImage(source: ImageSource.camera);
+                  onPressed: () async {
+                    final PickedFile file =
+                        await picker.getImage(source: ImageSource.camera);
+                    onImageSelected(File(file.path));
                   },
                   child: const Text('Camêra'),
                 ),
                 FlatButton(
-                  onPressed: () {
-                    picker.getImage(source: ImageSource.gallery);
+                  onPressed: () async {
+                    final PickedFile file =
+                        await picker.getImage(source: ImageSource.gallery);
+                    onImageSelected(File(file.path));
                   },
                   child: const Text('Galeria'),
                 ),
