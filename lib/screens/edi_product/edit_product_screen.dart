aadd3e7e6dc1719b6e7b976dab1e244c8536211f
FelipeFerreiraDev <felipeferreira.sp.dev@gmail.com>
2021-01-20 11:06:19 -0300

    Validando Imagem

--- lib/screens/edi_product/edit_product_screen.dart
+++ lib/screens/edi_product/edit_product_screen.dart
@@ -3,11 +3,12 @@ import 'package:BJDelivery/screens/edi_product/components/images_form.dart';
 import 'package:flutter/material.dart';
 
 class EditProductScreen extends StatelessWidget {
-
-  const EditProductScreen(this.product);
+  EditProductScreen(this.product);
 
   final Product product;
-  
+
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,10 +16,24 @@ class EditProductScreen extends StatelessWidget {
         title: const Text('Editar Anúncio'),
         centerTitle: true,
       ),
-      body: ListView(
-        children: <Widget>[
-          ImagesForm(product),
-        ],
+      backgroundColor: Colors.white,
+      body: Form(
+        key: formKey,
+        child: ListView(
+          children: <Widget>[
+            ImagesForm(product),
+            RaisedButton(
+              onPressed: () {
+                if(formKey.currentState.validate()){
+                  print('Válido!!!');
+                } else {
+                  print('Invalido');
+                }
+              },
+              child: const Text('Salvar'),
+            ),
+          ],
+        ),
       ),
     );
   }
